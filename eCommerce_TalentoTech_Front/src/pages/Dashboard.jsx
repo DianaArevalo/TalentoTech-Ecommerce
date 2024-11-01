04ef7f6ee860af70a51161ac8c76d97092c81549
DianaArevalo <morec9834@gmail.com>
2024-10-31 21:45:46 -0500

    CardHome - Responsive

--- eCommerce_TalentoTech_Front/src/pages/Dashboard.jsx
+++ eCommerce_TalentoTech_Front/src/pages/Dashboard.jsx
@@ -5,7 +5,7 @@ import Maps from '../components/Cards/Maps'
 
 const Dashboard = () => {
   return (
-    <div className='bg-red-300 w-full h-screen flex-row anyBox '>
+    <div className='bg-red-300 w-full h-screen flex-row anyBox justify-center '>
       <CardHome/>
      <Average/>
      <Maps/>
